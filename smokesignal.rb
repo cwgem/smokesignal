9037f7dc355af4721ed89170abf6b3b899f732ea
Chris White <cwhite@engineyard.com>
2013-05-06 18:15:53 -0700

    Refactoring Pass 1
    * Do a main loop which keeps itself alive for dealing with unhandled
      partial http responses which normally causes an exit
    * Pull functions into class structure
    * Added todo's for Pass 2 factoring

--- smokesignal.rb
+++ smokesignal.rb
@@ -2,11 +2,18 @@
 require 'json'
 require 'tinder'
 require 'escape'
+require 'etc'
 
 CONFIG_PATH = "#{File.dirname(__FILE__)}/config/campfire.json"
-LOG_PATH = "#{File.dirname(__FILE__)}/logs"
 config = JSON.parse(File.read(CONFIG_PATH))
+
+# TODO: Get this shoved into a class
 ICON_PATH=config['icon']
+LOG_PATH = "#{File.dirname(__FILE__)}/logs"
+
+def log_message(room_name, author, body)
+  File.open("#{LOG_PATH}/#{room_name}.log", "a") { |f| f.write("[#{Time.now.iso8601}] #{author}: #{body}\n") }
+end
 
 def dispatch_message(summary, body)
   
@@ -20,28 +27,66 @@ def dispatch_message(summary, body)
   system(Escape.shell_command(shell_raw))
 end
 
-def log_message(room_name, author, body)
-  File.open("#{LOG_PATH}/#{room_name}.log", "a") { |f| f.write("[#{Time.now.iso8601}] #{author}: #{body}\n") }
-end
+# TODO: Put this into a seperate file along with other gem structure
+module SmokeSignal
 
-campfire = Tinder::Campfire.new config['subdomain'], :token => config['token']
-threads = []
+  
+  class CampfireClient
+    def initialize(config)
+      @config = config
+      @campfire = Tinder::Campfire.new @config['subdomain'], :token => @config['token']
+      @rooms = []
+    end
 
-config['rooms'].each do | room |
-  room = campfire.find_room_by_name(room)
-  threads << Thread.new(room) { | campfire_room |
-    puts "Notify for #{campfire_room.name}"
-    campfire_room.listen do |message|
-      config['notifications'].each do |notification|
-        
-        if message[:body] =~ /#{Regexp.escape(notification)}/i
-          dispatch_message "New Campfire Message", "[#{campfire_room.name}] #{message[:user][:name]}: #{message[:body]}"
-          log_message campfire_room.name, message[:user][:name], message[:body]
+    def listen_on_rooms()
+      @config['rooms'].each do | room_name | 
+        room_search_result = @campfire.find_room_by_name(room_name)
+        # TODO: figure out better method of dealing with this
+        setup_listener_thread(room_search_result) unless room_search_result == nil
+      end
+      
+      @rooms.each { | room | room.thread.join }
+    end
+
+    def setup_listener_thread(room)
+      room_listener = RoomListener.new(room, @config['notifications'])
+      room_listener.thread = Thread.new(room_listener) { | campfire_room | campfire_room.listen }
+      @rooms << room_listener
+    end
+
+  end
+
+  class RoomListener
+    attr_accessor :thread
+
+    def initialize(room, notifications)
+      @room = room
+      @thread = nil
+      @notifications = notifications
+    end
+
+    def listen()
+      puts "Listening on #{@room.name}"
+      @room.listen do | message |
+        @notifications.each do | notification |
+          if message[:body] =~ /#{Regexp.escape(notification)}/i
+            dispatch_message "New Campfire Message", "[#{@room.name}] #{message[:user][:name]}: #{message[:body]}"
+            log_message @room.name, message[:user][:name], message[:body]
+          end
         end
-
       end
     end
-  }
+
+  end
+
 end
 
-threads.each { |aThread| aThread.join }
+# TODO: Figure out the specific exception to handle against
+#       for partial HTTP request errors
+loop do
+  begin
+    client = SmokeSignal::CampfireClient.new config
+    client.listen_on_rooms
+  rescue
+  end
+end
